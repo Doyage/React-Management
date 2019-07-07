4fcf765ee32d237898e02770ebc419506e3e063c
Doyage <pcstt@naver.com>
2019-07-07 15:17:43 +0900

    Create  Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birth': '890823',
+    'sex': 'male',
+    'job': 'stu'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동2',
+    'birth': '890824',
+    'sex': 'female',
+    'job': 'stu2'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '홍길동3',
+    'birth': '890825',
+    'sex': 'male',
+    'job': 'stu3'
+  }
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's dev management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birth={c.birth}
+              sex={c.sex}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
